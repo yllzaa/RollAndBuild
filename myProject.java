1a5b1caad4765f5ec116cfbd004ad4c97b8ac2fe
Yllza Kryeziu <yllza.kryeziu@student.uni-pr.edu>
2023-03-30 09:44:23 +0200

    add the starter parts in main

--- myProject.java
+++ myProject.java
@@ -8,4 +8,28 @@ public class myProject {
 	private static String door = "\n  /\\ \n /  \\ \n------ \n|    | \n| || | \n------";
 	private static String window1 = "\n  /\\ \n /  \\ \n------ \n|    | \n| ||x| \n------";
 	private static String window2 = "\n  /\\ \n /  \\ \n------ \n|    | \n|x||x| \n------";
+
+
+	public static void main(String[] args) {
+		 
+		/** */
+		RollAndBuild Player1 = new RollAndBuild();
+		RollAndBuild Player2 = new RollAndBuild();
+		/** the scanner will alow us to throw the dice on command */
+		Scanner scan = new Scanner(System.in);
+		
+		house1 = "";
+		house2 = "";
+		/** the die we will use the method "roll" on */
+		int die;
+		/** the value which will let us switch between players */
+		int playerSwitching = 0;
+		
+		
+		System.out.println("Hello! Welcome to our game!");
+		System.out.println("You have to roll the dice to build a house!");
+		System.out.println("Type something to start! \n");
+		
+
+
 }
