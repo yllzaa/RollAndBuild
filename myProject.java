1c1927e9d693fa7978cc2ea1ab96a067574b774a
Yllza Kryeziu <yllza.kryeziu@student.uni-pr.edu>
2023-03-30 09:23:20 +0200

    create class and add house strings

---
+++ myProject.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class myProject {
+	private static String house1;
+	private static String house2;
+	private static String base = "\n|     | \n|     | \n ------";
+	private static String roof = "\n  /\\ \n /  \\ \n------ \n|    | \n|    | \n------";
+	private static String door = "\n  /\\ \n /  \\ \n------ \n|    | \n| || | \n------";
+	private static String window1 = "\n  /\\ \n /  \\ \n------ \n|    | \n| ||x| \n------";
+	private static String window2 = "\n  /\\ \n /  \\ \n------ \n|    | \n|x||x| \n------";
+}
